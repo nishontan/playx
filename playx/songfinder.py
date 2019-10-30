39cc52c450e9efcc46bd991f09f0a7c0ed12417d
Nishon Tandukar <nishon.tan@gmail.com>
2019-10-30 13:57:09 +0545

    refactors search_with_exclude (#1)

--- playx/songfinder.py
+++ playx/songfinder.py
@@ -8,6 +8,7 @@ from playx.stringutils import (
 )
 import random
 
+
 def search(song, no_kw_in_search):
     """Search the song in youtube."""
     videos = search_youtube(song, no_kw_in_search)
@@ -19,18 +20,18 @@ def search(song, no_kw_in_search):
 
 
 def search_with_exclude(song, songs_to_exclude):
-    """Search the song in youtube."""
+    """Search the song in youtube and remove songs passed in songs_to_exclude"""
     videos = search_youtube(song, skip_kw=True)
     string_list = []
     for video in videos:
         fixed_title = fix_title(video.title)
         string_list.append(fixed_title)
-    
-    to_remove = get_closest_match_ignorecase(string_list,songs_to_exclude[0])
-    index = string_list.index(to_remove)
 
-    del string_list[index]
-    del videos[index]
+    if songs_to_exclude:
+        item_to_remove = get_closest_match_ignorecase(string_list, songs_to_exclude[0])
+        if item_to_remove:
+            index = string_list.index(item_to_remove)
+            del string_list[index]
+            del videos[index]
 
-    print(index)
     return random.choice(videos)
